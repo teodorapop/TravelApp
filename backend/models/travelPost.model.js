7e6b2427513e5337b79c2c5b8bd832ec8020b4ba
Teo <teodorapop84@yahoo.com>
2025-02-14 19:52:27 +0200

    first backend

---
+++ backend/models/travelPost.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const travelPostSchema = new Schema({
+    title:{type:String,required: true},
+    story:{type:String,required: true},
+    visitedLocation:{type:[String], default:[]},
+    isFavourite:{type:Boolean, default: false},
+    userId:{type:Schema.Types.ObjectId, ref:'User', required:true},
+    createdOn:{type:Date, default:Date.now},
+    imageUrl:{type:String, required:true},
+    visitedDate:{type:Date, required:true},
+    isPublic:{type:Boolean, default: false},
+});
+
+module.exports = mongoose.model("TravelPost", travelPostSchema);
+
