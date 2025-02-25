a4b5ac51e3243b53643e415ef33205e0ee8138ea
teodorapop <82906086+teodorapop@users.noreply.github.com>
2025-02-25 12:17:05 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: [""],
+        origin: ["htps://travel-app-frontend-sooty.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true,
     }
