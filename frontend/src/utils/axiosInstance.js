c4b19015e43783b9984d305914b913c5875454ca
teodorapop <82906086+teodorapop@users.noreply.github.com>
2025-02-25 15:30:46 +0200

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://travel-app-api-drab.vercel.app',
+    baseURL: 'https://travel-app-backend-7eko.onrender.com',
     timeout: 10000,
     headers: {
         "Content-Type": "application/json"
