843b3d45fc32a873de2ada78830a6c0b4518a5b8
teodorapop <82906086+teodorapop@users.noreply.github.com>
2025-02-26 15:38:30 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,6 @@ const Navbar = ({userInfo, searchQuery, setSearchQuery, onSearchPost, handleClea
     return (
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
             <img src={LOGO} alt="logo" className="h-14"/>
-            <img src={"../../assets/images/TravelJourney.png"} alt="another logo" className="h-14"/>
 
             {isToken && (
                 <>
