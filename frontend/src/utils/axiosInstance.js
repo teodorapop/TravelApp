167f91bc6859d52ea3c74ad94f3bf28e519c9f7c
teodorapop <82906086+teodorapop@users.noreply.github.com>
2025-02-26 14:10:59 +0200

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://travel-app-backend-7eko.onrender.com',
-    timeout: 10000,
+    // timeout: 10000,
     headers: {
         "Content-Type": "application/json"
     },
